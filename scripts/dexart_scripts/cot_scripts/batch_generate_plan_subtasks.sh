a7692c4dd5de34471794e1c96dbc092bc47e6e52
LYDLYDLYDLYDLYD <107243816+LYDLYDLYDLYDLYD@users.noreply.github.com>
2025-06-24 21:34:27 +0800

    Update batch_generate_plan_subtasks.sh

--- scripts/dexart_scripts/cot_scripts/batch_generate_plan_subtasks.sh
+++ scripts/dexart_scripts/cot_scripts/batch_generate_plan_subtasks.sh
@@ -2,7 +2,7 @@
 
 export GOOGLE_API_KEY=
 
-python ./cot_code/batch_generate_plan_subtasks.py \
+python ./scripts/dexart_scripts/cot_scripts/batch_generate_plan_subtasks.py \
     --libero_dataset_dir ./results/bucket \
     --libero_primitives_path ./data/bucket/primitives.json \
     --libero_scene_desc_path ./data/bucket/descriptions_bucket_fixed.json \
